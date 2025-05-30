8d0643633f639159598f4f733c496c7b92156325
Skjesper <jesperskeppstedt@gmail.com>
2025-05-30 11:15:06 +0200

    Updated page to include mock buttons for development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 
 import Box from '@/components/Puzzle/Box/Box';
-
+import Button from '@/components/ui/Button/Button';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Box>
-        Hej
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
+        <Button variant='m'>1</Button>
       </Box>
     </div>
   );
