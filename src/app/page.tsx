2748385a44466489ed00f83d67170eaa08b646dc
Skjesper <jesperskeppstedt@gmail.com>
2025-05-30 10:58:07 +0200

    built a basic first box grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 
 
+import Box from '@/components/Puzzle/Box/Box';
 import Button from '@/components/ui/Button/button';
+
 
 export default function Home() {
   return (
     
-      <Button variant="m">Test</Button>
+     
+
+      <Box>
+         <Button variant="m">Test</Button>
+      </Box>
    
   );
 }
--- src/components/Puzzle/Box/box.tsx
+++ src/components/Puzzle/Box/box.tsx
@@ -3,5 +3,15 @@ import React from 'react';
 
 interface BoxProps {
 
-    
+    children: React.ReactNode;
 }
+
+const Box: React.FC<BoxProps> = ({ children }) => {
+    return (
+        <div className={styles.box}>
+            {children}
+        </div>
+    );
+};
+
+export default Box;
